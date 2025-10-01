c87153ad9359ec6a614b37a1ca8af7222a0d917b
PedroTM <pedrogames090@gmail.com>
2025-10-01 11:30:35 -0300

    New - Front (css)

--- themes/adm/_theme.php
+++ themes/adm/_theme.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Área Administrativa</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="<?= url("assets/css/_shered/theme.css"); ?>">
-    <link rel="stylesheet" href="<?= url("assets/css/_shered/global.css"); ?>">
+    <link rel="stylesheet" href="<?= url("assets/css/_shared/main.css"); ?>">
+    <link rel="stylesheet" href="<?= url("assets/css/_shared/global.css"); ?>">
     <script type="module" src="<?= url("assets/js/admin/home.js"); ?>" async></script>
     
     <?php if ($this->section("home")): ?>
