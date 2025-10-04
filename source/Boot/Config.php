22deddb26be69ca89dbf47d78153ca374422ccc1
LCVitor <vitorfomezera@gmail.com>
2025-10-04 07:53:54 -0300

    New - Config.php e .env

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -4,7 +4,7 @@ const CONF_DB_HOST = "localhost";
 const CONF_DB_USER = "root";
 const CONF_DB_PASS = "";
 const CONF_DB_NAME = "db_SarauWeb";
-const CONF_DB_PORT = "3306";
+const CONF_DB_PORT = "3311";
 
 const CONF_URL_TEST = "http://localhost/Sarau-Web/";
 const CONF_URL_BASE = "http://localhost/Sarau-Web/";
