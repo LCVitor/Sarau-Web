a22e103d3d478a29ce5cdeba065db9f9c867ff80
PedroTM <pedrogames090@gmail.com>
2025-10-07 11:14:21 -0300

    Front - modais de indeferimento e informações (Admin)

--- themes/adm/events.php
+++ themes/adm/events.php
@@ -9,42 +9,71 @@ $this->start("events");
 <?php
 $this->end();
 ?>
-
-<div class="title-area">
-    <h1>Eventos</h1>
-</div>
-
-<div class="actions">
-    <div class="event">
-        <h3>Iniciar um novo evento</h3>
-        <button class="button type-1">Novo evento</button>
-        <div> Histórico (?!) </div>
+<body>    
+    <div class="title-area">
+        <h1>Eventos</h1>
     </div>
-    <div class="enrollment">
-        <h3>Incrições</h3>
-        <p>Visualização e seleção dos inscritos</p>
-        <div class="container-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Descrição</th>
-                        <th>Tempo de apresentação (min)</th>
-                        <th>Setor</th>
-                        <th>Artista</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody></tbody>
-            </table>
 
-            <div class="pagination">
-                <button disabled>&laquo; Anterior</button>
-                <button class="active">1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>Próxima &raquo;</button>
+    <div class="actions">
+        <div class="event">
+            <h3>Iniciar um novo evento</h3>
+            <button class="button type-1">Novo evento</button>
+            <div> Histórico (?!) </div>
+        </div>
+        <div class="enrollment">
+            <h3>Incrições</h3>
+            <p>Visualização e seleção dos inscritos</p>
+            <div class="container-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Descrição</th>
+                            <th>Tempo de apresentação (min)</th>
+                            <th>Setor</th>
+                            <th>Artista</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+
+                <div class="pagination">
+                    <button disabled>&laquo; Anterior</button>
+                    <button class="active">1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>Próxima &raquo;</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div id="container-modal-type-1" id="info-area">
+        <div class="modal">
+            <h1>Informações do evento</h1>
+            <button id="close-btn" class="button type-2">&times;</button>
+            <div id="event-info">
+                <p>ID do evento: <b>12345</b></p>
+                <p>Apresentador: <b>Exemplo</b></p>
+                <p>Setor artístico: <b>Exemplo</b></p>
+                <p>Duração: <b>00:00</b></p>
+                <p>Observação: <b>Exemplo</b></p>
+                <p>Status: <b id="event-state">Não avaliado</b></p>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div id="container-modal-type-1" id="defer-area">
+        <div class="modal">
+            <h1>Indeferir evento</h1>
+            <button id="close-btn" class="button type-2">&times;</button>
+            <form id="form-registration" class="form">
+                <p>AVISO: Eventos indeferidos só poderam ser acessados denovo caso o apresentador o reenvie. Indeferir um evento é permanente.</p>
+                <label>Motivo: </label>
+                <textarea></textarea>
+                <input type="submit" class="button type-3" value="Deferir">
+            </form>
+        </div>
+    </div> -->
+</body>
